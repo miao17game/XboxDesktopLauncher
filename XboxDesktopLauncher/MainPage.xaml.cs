e389ec1be1d1a3ac3e72e7b37a90b00270003a53
bigmogician <bigmogician@outlook.com>
2020-06-01 20:33:48 +0800

    feat: use app router and param struct

--- XboxDesktopLauncher/MainPage.xaml.cs
+++ XboxDesktopLauncher/MainPage.xaml.cs
@@ -4,14 +4,15 @@ using System.Diagnostics;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
+using XboxDesktopLauncher.Utils;
+
 using muxc = Microsoft.UI.Xaml.Controls;
-using pages = XboxDesktopLauncher.Pages;
 
 namespace XboxDesktopLauncher {
 
-    internal class Parameter {
+    internal struct Parameter {
 
-        public string Tag { get; set; }
+        public readonly string Tag;
 
         public Parameter(string tag) {
             Tag = tag;
@@ -19,13 +20,6 @@ namespace XboxDesktopLauncher {
     }
 
     public sealed partial class MainPage : Page {
-
-        private readonly Dictionary<string, Type> routes = new Dictionary<string, Type> {
-            {"home" , typeof(pages.HomePage) },
-            {"games" , typeof(pages.GamesPage) }
-        };
-
-        private Type DefaultRoute { get { return routes["home"]; } }
 
         private string Current { get; set; }
 
@@ -38,17 +32,13 @@ namespace XboxDesktopLauncher {
             var tag = args.InvokedItemContainer?.Tag.ToString();
             var current = (muxc.NavigationViewItem)NavigationBar.SelectedItem;
             if (Current == current.Tag.ToString()) return;
-            ContentFrame.NavigateToType(UsePage(tag), new Parameter(tag), new FrameNavigationOptions {
+            ContentFrame.NavigateToType(ApplicationRouter.GetRoute(tag), new Parameter(tag), new FrameNavigationOptions {
                 TransitionInfoOverride = args.RecommendedNavigationTransitionInfo
             });
         }
 
-        private Type UsePage(string tag) {
-            return routes[tag ?? "default"] ?? DefaultRoute;
-        }
-
         private void OnNavigated(object sender, NavigationEventArgs args) {
-            Current = ((Parameter)args.Parameter)?.Tag;
+            Current = ((Nullable<Parameter>)args.Parameter)?.Tag;
         }
     }
 }
---
+++ XboxDesktopLauncher/Utils/ApplicationRouter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using pages = XboxDesktopLauncher.Pages;
+
+namespace XboxDesktopLauncher.Utils {
+    public static class ApplicationRouter {
+
+        public static Dictionary<string, Type> routes = new Dictionary<string, Type> {
+            {"home" , typeof(pages.HomePage) },
+            {"games" , typeof(pages.GamesPage) }
+        };
+
+        public static Type DefaultRoute { get { return routes["home"]; } }
+
+        public static Type GetRoute(string tag) {
+            return routes[tag ?? "default"] ?? DefaultRoute;
+        }
+
+    }
+}
