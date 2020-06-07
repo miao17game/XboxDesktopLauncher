e600589f6239b839751a3cc24e7769eba0eeb8bf
bigmogician <bigmogician@outlook.com>
2020-06-07 16:47:34 +0800

    feat: add package query code

--- XboxDesktopLauncher/Pages/HomePage.xaml.cs
+++ XboxDesktopLauncher/Pages/HomePage.xaml.cs
@@ -1,27 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Management.Deployment;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+using XboxDesktopLauncher.Utils;
 
 namespace XboxDesktopLauncher.Pages {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
+
+    public class WinPackage {
+        public string Logo;
+        public string Name;
+    }
+
     public sealed partial class HomePage : Page {
+
+        private readonly PackageManager manager = new PackageManager();
+
+        public ObservableCollection<WinPackage> Packages = new ObservableCollection<WinPackage> { };
+
         public HomePage() {
             this.InitializeComponent();
         }
+
+        private void LoadPackages() {
+            Task.Run(async () => {
+                var packages = manager.FindPackagesForUser("")
+                    .OrderBy((p) => p.DisplayName)
+                    .Where(p =>
+                        p.SignatureKind == PackageSignatureKind.Store &&
+                        !p.IsDevelopmentMode &&
+                        !p.IsResourcePackage &&
+                        !p.Status.NotAvailable)
+                    .Select(pack => new WinPackage { Name = pack.DisplayName ?? pack.Id.Name, Logo = pack.Logo.AbsoluteUri })
+                    .ToList();
+                await UI.UpdateAsync(() => {
+                    Packages.Clear();
+                    foreach (var package in packages) {
+                        Packages.Add(package);
+                    }
+                });
+            });
+        }
+
+        private void PackageList_Loaded(object sender, RoutedEventArgs e) {
+            LoadPackages();
+        }
+
     }
 }
---
+++ XboxDesktopLauncher/Utils/UI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace XboxDesktopLauncher.Utils {
+    public static class UI {
+
+        public static async Task UpdateAsync(DispatchedHandler handler) {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, handler);
+        }
+
+    }
+}
